ebf6c9b104ce5343951b7632fa10a83de816a16e
NeoxAzrot1 <sami.lepays@gmail.com>
2021-02-21 15:34:12 +0100

    push for elise

---
+++ src/components/Cursor/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from './Cursor.module.sass'
+
+const Cursor = () => {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  )
+}
+
+export default Cursor
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from './Menu.module.sass'
+import { NavLink } from 'react-router-dom'
+import Burger from 'icons/Burger'
 
 const Menu = () => {
-  return (
-    <div className={styles.container}>
+  const [menuIsShow, setMenuIsShow] = useState(false)
 
-    </div>
+  const toggleMenu = () => {
+    setMenuIsShow(!menuIsShow)
+  }
+  
+  return (
+    <nav className={styles.container}>
+      <div className={styles.menu__container}>
+        <ul>
+          <li>
+            <NavLink to="/all-projects" className={styles.menu__link}>
+              all projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/services" className={styles.menu__link}>
+              services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className={styles.menu__link}>
+              about
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className={styles.menu__link}>
+              contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div onClick={toggleMenu}>
+        <Burger isShow={menuIsShow}/>
+      </div>
+    </nav>
   )
 }
 
