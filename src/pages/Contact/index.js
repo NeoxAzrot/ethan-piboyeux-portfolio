f822c7c266644ea13f6baab6225a2bf18d893ebf
EliiiseE <elise.echasseriau@mmibordeaux.com>
2021-02-22 11:51:33 +0100

    about + responsive

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Layout from 'components/Layout'
 import styles from './Contact.module.sass'
+import Footer from 'components/Footer'
 
 const Contact = () => {
   return (
     <Layout>
       <div className={styles.container}>
         <a href="mailto:contact (@) ethanpiboyeux.fr">contact (@) ethanpiboyeux.fr</a>
-      </div>
+      </div> 
+      <Footer/>
     </Layout>
+
+   
   )
 }
 
